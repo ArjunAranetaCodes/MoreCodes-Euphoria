f68779dbdfcca35fe07ea2c9200451127ddda586
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-03 08:08:06 +0800

    2018 Updates

---
+++ Conversions/problem10.ex
@@ -0,0 +1,15 @@
+//Problem 10: Write a program that converts a decimal number to octal number.
+include std/sequence.e
+include std/math.e
+include std/convert.e
+
+integer dec = 256
+sequence oct = ""
+
+while dec > 0 do
+ oct = to_string(to_integer(remainder(dec,8))) & oct
+ dec = to_integer(dec / 8)
+end while
+
+puts(1, oct)
+
