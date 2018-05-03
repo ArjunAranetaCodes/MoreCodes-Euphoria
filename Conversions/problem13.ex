d7c44c002d3b5bdcedb009420f5c81ea00a28d8a
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-04 07:18:51 +0800

    2018 Updates

---
+++ Conversions/problem13.ex
@@ -0,0 +1,10 @@
+//Problem 13: Write a program that converts celsius to farenheit.
+include get.e
+
+atom celsius, farenheit
+
+celsius = prompt_number("Enter value of celsius: ", {})
+farenheit = (9.0 / 5.0) * celsius + 32.0
+
+puts(1, '\n')
+printf(1, "%fC equals to %fF \n", {celsius, farenheit})
