c0a1ef7e82c1eee19352bb5714acaf2c9ae63b8a
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-04 07:20:25 +0800

    2018 Updates

---
+++ Conversions/problem20.ex
@@ -0,0 +1,8 @@
+//Problem 20: Write a program that converts a number to its 
+//corresponding month (e.g. 1 = January).
+integer index = 1
+sequence month = {"January", "February", "March", "April",
+  "May", "June", "July", "August",
+  "September", "October", "November", "December"}
+
+puts(1, month[index])
