b3035b1b0335fd59fb14aa5cffc1c849d159e8ac
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-03 08:07:04 +0800

    2018 Updates

---
+++ Conversions/problem5.ex
@@ -0,0 +1,7 @@
+//Problem 5: Write a program that converts an array/list to string.
+include std/sequence.e
+
+sequence arrNumbers= {"1", "2", "3"}
+sequence numbers = join(arrNumbers, ",")
+
+puts(1, numbers)
