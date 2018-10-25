b04c785bc61a46c8d025f65f5744094da8a4ad7f
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-10-25 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/problem2.ex
@@ -0,0 +1,18 @@
+/*
+Problem 2: Write a program to print the number pattern of ones and zeros using loop.
+11111
+00000
+11111
+00000
+11111
+*/
+for y = 0 to 4 do
+ for x = 0 to 4 do 
+  if remainder(y, 2) = 0 then
+   puts(1, "1")
+  else
+   puts(1, "0")
+  end if 
+ end for
+ puts(1, "\n")
+end for
