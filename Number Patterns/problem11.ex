db0f34ea2cf697ca4e143e8e1b4f4be0ebad3cae
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-10-26 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/problem11.ex
@@ -0,0 +1,14 @@
+/*
+Problem 11: Write a program to print the pattern of asterisks using loop.
+*
+**
+***
+****
+*****
+*/
+for y = 0 to 4 do
+ for x = 0 to y do 
+  puts(1, "*")
+ end for 
+ puts(1, "\n")
+end for 
